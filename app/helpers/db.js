f449ff27d52c74c80417ff83ade1aa56e3ecd802
nguyentranhau98 <nguyentranhau98@gmail.com>
2019-07-04 16:22:07 +0700

    redeploy

--- app/helpers/db.js
+++ app/helpers/db.js
@@ -1,6 +1,6 @@
 const config = require('../configs/config.json');
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || config.connectionString, { useCreateIndex: true, useNewUrlParser: true });
+mongoose.connect("mongodb://vuong:vuong123@ds263161.mlab.com:63161/music-app", { useCreateIndex: true, useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 
 module.exports = {
